0bcc3c771267d0157cdb393f3b3e6829a2162023
SimonGideon <simongideon918@gmail.com>
2022-03-23 11:37:38 +0300

    sum up function

--- functions.js
+++ functions.js
@@ -3,4 +3,11 @@ function square(num){
     let squared = num*num;
     console.log(squared);
 }
-square(50000000);
+square(50);
+
+// finding the sum function.
+function sum(num1, num2){
+    const add = num1 + num2;
+    console.log(add);
+}
+sum(75, 50);
