7bc8889ac77a2daa02e7abe6904b1b0d6b73c723
SimonGideon <simongideon918@gmail.com>
2022-04-04 16:04:10 +0300

    dai;y free code challange

--- lang.js
+++ lang.js
@@ -1,15 +1,31 @@
+// function translatePigLatin(str) {
+//   vowel = ['a', 'e', 'i', 'o', 'u'];
+//   for(i=0; i<str.length; i++){
+//   let deleted = str.substring([i]);
+//   append = deleted + str[i]+"ay";
+//   present = vowel.includes(str[i]);
+//   if(present == true){
+//     return console.log(str + "way");
+//   } else{
+//     return console.log(append);
+//   }
+//   }
+// }
+
+// translatePigLatin("kkalgorithm");
+// =======><========
 function translatePigLatin(str) {
-  vowel = ['a', 'e', 'i', 'o', 'u'];
-  for(i=0; i<str.length; i++){
-  let deleted = str.substring([i]);
-  append = deleted + str[i]+"ay";
-  present = vowel.includes(str[i]);
+  let vowel = ['a', 'e', 'i', 'o', 'u'];
+  let deleted = str.substring(1);
+  let present = vowel.includes(str[0]);
   if(present == true){
     return console.log(str + "way");
-  } else{
-    return console.log(append);
-  }
+  }else{
+    for(let i=0; i<str.length; i++){
+  if(vowel.includes(str[i])){
+            return console.log(str.slice(i, str.length) + str.substring(0, i) + 'ay');   
+   }
+}
   }
 }
-
-translatePigLatin("kkalgorithm");
+translatePigLatin("schwartz");
