c9c3ea4a508c839f2a559f0372f248326154d5b7
SimonGideon <simongideon918@gmail.com>
2022-04-17 18:20:04 +0300

    Hackersrank Plus Minus Challange

---
+++ plus_minus.js
@@ -0,0 +1,20 @@
+function plusMinus(arr) {
+var count = arr.length;
+var posval = 0;
+var negval = 0;
+var zero = 0;
+  for(let i=0; i<count; i++){
+      if(arr[i]<0){
+        negval+=1;
+      }else if(arr[i]>0){
+        posval+=1;
+      }else{
+        zero+=1;
+      }
+  }
+var pcal = posval/count;
+var ncal = negval/count;
+var zerocal = zero/count;
+console.log(pcal.toFixed(6)+ "\n" +ncal.toFixed(6)+'\n'+zerocal.toFixed(6) + '\n')
+  
+}
