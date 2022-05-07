57014c5088c783a46f3315cf5880b6fb1d1757e0
SimonGideon <simongideon918@gmail.com>
2022-05-07 21:17:17 +0300

    Js

---
+++ variable.js
@@ -0,0 +1,7 @@
+// variable is a spce preserved for a character in the memory
+let name = "John"; 
+console.log(name);
+let score = 100;
+let bonus = 20;
+let total_score = score+bonus;
+console.log(total_score);
