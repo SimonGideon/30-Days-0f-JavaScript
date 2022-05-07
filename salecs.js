4cbee3a47162cb692fa42a7dd3ff2ce7903c13ab
SimonGideon <simongideon918@gmail.com>
2022-05-07 22:19:20 +0300

    initial commit

---
+++ salecs.js
@@ -0,0 +1,22 @@
+var score = 100;
+score = 130;
+const bonus = 50;
+var finalScore = (score+bonus) * 1.8;
+console.log(finalScore)
+    var complex = ((4+4)*5)/2
+console.log(complex)
+
+// convert cencious to Farenheit
+let tempInFarenheit = 32;
+// calculation
+var temp = 0;
+console.log("Current temprature " + temp);
+// grade sysytem
+// 10 - great job
+// 5 - Work harder
+// 2 - poor
+// 0 - fail
+let grade = true;
+let actualMarks =7;
+let myGrade = (actualMarks < 10);
+console.log(myGrade);
