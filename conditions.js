a2ba0eb2e139db2bc275ca8f75d05f5a5e536f42
SimonGideon <simongideon918@gmail.com>
2022-05-07 22:35:46 +0300

    Initial commit

--- conditions.js
+++ conditions.js
@@ -21,3 +21,20 @@ if(whoIsHere==='user'){
 
 
 }
+// grade problem
+//  10 -Amaizing;
+// 5 - Good
+// 3 - Poor
+// 0 - Fail
+function grade(marks)
+{if(marks==10){
+    console.log('Amaizing');
+} if (marks==5) {
+    console.log('Good')
+    
+} if (marks==3) {
+    console.log("Poor")
+} if(marks==0) {
+    console.log("Poor")
+}}
+grade(10);
