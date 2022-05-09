5e5571ed0e1060db5613535031ece00f5df35364
SimonGideon <simongideon918@gmail.com>
2022-05-09 16:21:39 +0300

    Initial commit

--- function.js
+++ function.js
@@ -16,3 +16,14 @@ function myAdder(num1, num2) {
     return added;
 }
 console.log(myAdder(1232, 353453))
+function myMultiplier(num1, num2){
+    return num1*num2;
+}
+console.log(myMultiplier(12, 12))
+
+// course count
+function guestUser(name = 'unName', courseCount = 0){
+    return 'Hello ' + name + ' and your course is: ' + courseCount
+
+}
+console.log(guestUser('John', 1));
