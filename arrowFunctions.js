d25019225d1854052ffdccb8732bf94371fa56a2
SimonGideon <simongideon918@gmail.com>
2022-05-10 23:49:30 +0300

    Initial commit

--- arrowFunctions.js
+++ arrowFunctions.js
@@ -1,32 +1,42 @@
-// function sayHello(name) {
-//     return "Hey there, " + name + "!";   
-// }
-// console.log(sayHello("Summy"))
- const sayHello = (name) => `Hey there, ${name} !`;    
-console.log(sayHello("Summy"));
-const todos = [{
-    title: 'Buy Bread',
-    isDone: true,
+// // function sayHello(name) {
+// //     return "Hey there, " + name + "!";   
+// // }
+// // console.log(sayHello("Summy"))
+//  const sayHello = (name) => `Hey there, ${name} !`;    
+// console.log(sayHello("Summy"));
+// const todos = [{
+//     title: 'Buy Bread',
+//     isDone: true,
     
-},
-{
-    title: 'Attend an online class',
-    isDone: false,
-},{
-    title: 'Read an Agile code book',
-    isDone: false,
+// },
+// {
+//     title: 'Attend an online class',
+//     isDone: false,
+// },{
+//     title: 'Read an Agile code book',
+//     isDone: false,
 
-},{
-    title: 'Meditation',
-    isDone: false,
-},
-{
-    title: 'Go to gym',
-    isDone: true,
-}, {
-    title: 'Record Youtube Video',
-    isDone: false,
-}]
+// },{
+//     title: 'Meditation',
+//     isDone: false,
+// },
+// {
+//     title: 'Go to gym',
+//     isDone: true,
+// }, {
+//     title: 'Record Youtube Video',
+//     isDone: false,
+// }]
 
-const thingsDone = todos.filter((todo)=> todo.isDone ===false)
-console.log(thingsDone(title));
+// const thingsDone = todos.filter((todo)=> todo.isDone ===false)
+// console.log(thingsDone(title));
+const cameras = {
+    price: 600,
+    weight: 2000,
+    myDes: function(){
+        return `This canon camera is of ${this.price}`
+    }
+}
+console.log(cameras.myDes())
+
+// const func=() => ({key: 'value'});
---
+++ coersion.js
@@ -0,0 +1,3 @@
+// console.log('5');
+// // const giveType = typeof(5);
+// console.log(giveType)
