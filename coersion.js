0f5ae40b7394c3b259b93d8b92a30758fc501ce1
SimonGideon <simongideon918@gmail.com>
2022-05-11 17:43:54 +0300

    Initial commit

--- coersion.js
+++ coersion.js
@@ -1,3 +1,14 @@
 // console.log('5');
 // // const giveType = typeof(5);
 // console.log(giveType)
+// const adder = true +5
+// console.log(adder);
+
+const loginDetails = [];
+// login for getting details from DB
+const loginID = loginDetails[0];
+if (loginID) {
+    console.log('Allow user to login');
+} else{
+    console.log('Auth failed')
+}
