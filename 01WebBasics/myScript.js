be2905db2925c881adca92c6137598af3d6cdb59
SimonGideon <simongideon918@gmail.com>
2022-05-12 16:13:58 +0300

    Getting started wit DOM

--- 01WebBasics/myScript.js
+++ 01WebBasics/myScript.js
@@ -1,1 +1,8 @@
-alert("File is attached")
+// const myElemnt = document.querySelectorAll("#idone");
+// console.log(myElemnt)
+// const myPElements = document.querySelector('p')
+// myPElements.textContent = 'I am beign changed using js'
+// console.log(myPElements)
+
+const myPElements = document.querySelectorAll('p')
+myPElements.forEach((p)=> p.textContent = 'I am changed using js Loop')
